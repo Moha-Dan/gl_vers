7617119971033a740b71e65c6d92d48621c719d0
Modan <mdan45941@gmail.com>
2020-11-04 11:36:52 +0100

    Test de la norme

--- src/test/testVecteur.java
+++ src/test/testVecteur.java
@@ -8,21 +8,21 @@ public class testVecteur {
 	@Test
 	public void testVecteur() {
 		
-		// fail("Not yet implemented");
+		fail("Not yet implemented");
 		boolean test = false;
 		// test/1
-		//vecteur v = new vecteur(1,1);
-		//test = ( v.getx()==1 && v.gety()==1 ) ;
+		vecteur v = new vecteur(1,1);
+		test = ( v.getx()==1 && v.gety()==1 ) ;
 		assertTrue(test);
 	}
 
 	@Test
 	public void testNorme() {
-		//fail("Not yet implemented");
+		fail("Not yet implemented");
 		boolean test = false;
 		// test/2
-		// vecteur v = new vecteur(1, 1);
-		// test = ( ( Math.sqrt(2) - v.norme() ) < 1E-10 );
+		 vecteur v = new vecteur(1, 1);
+		 test = ( ( Math.sqrt(2) - v.norme() ) < 1E-10 );
 		assertTrue( test );
 	}
 
